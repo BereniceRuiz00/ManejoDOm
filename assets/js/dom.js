8f52afd6e5196f67b5da807c675c0908abc7554c
BereniceRuiz00 <ruizsilviabere@gmail.com>
2022-08-08 16:47:54 -0500

    Se crea nuevo repositorio con ejercicios js sobre manipulacion del DOM, primer ejercicio es por medio de js incrustar texto en dos parrafos

---
+++ assets/js/dom.js
@@ -0,0 +1,5 @@
+
+let parrafo = "HOla Bere";
+let parrafo2 = document.getElementById("parrafo").innerHTML = parrafo;
+
+const cambiodePArrafo = document.getElementById("parrafo2").innerHTML = "quiero chocolate";
